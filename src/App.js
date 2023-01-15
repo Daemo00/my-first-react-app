5144d3d6075ecd054fdc2d0c03bd396c8bd73971
Simone Rubino <daemo00@gmail.com>
2023-01-15 19:37:28 +0100

    Board and type checking

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
-export default function Square() {
-  return <button className="square">X</button>;
+function Square({value}) {
+  return <button className="square">{value}</button>;
+}
+Square.propTypes = {
+  value: PropTypes.string.isRequired,
+};
+
+export default function Board() {
+  return (
+    <>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </>
+  );
 }
