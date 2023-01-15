5e77430f66f571b814153a61f5af7badc7c94e16
Simone Rubino <daemo00@gmail.com>
2023-01-15 19:52:29 +0100

    State management

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import React, {useState} from 'react';
 
-function Square({value}) {
+function Square() {
+  const [value, setValue] = useState(null);
   function handleClick() {
-    console.log('clicked!');
+    setValue('X');
   }
   return <button
     className="square"
@@ -12,27 +12,24 @@ function Square({value}) {
     {value}
   </button>;
 }
-Square.propTypes = {
-  value: PropTypes.string.isRequired,
-};
 
 export default function Board() {
   return (
     <>
       <div className="board-row">
-        <Square value="1" />
-        <Square value="2" />
-        <Square value="3" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="4" />
-        <Square value="5" />
-        <Square value="6" />
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <Square value="7" />
-        <Square value="8" />
-        <Square value="9" />
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
